a85b4e09a34741d9026700e0cc5232029ff0170d
@ <studiodeveloper@hotmail.com>
2025-11-26 21:17:40 -0800

    production push

--- storefront/src/modules/home/components/video-section/index.tsx
+++ storefront/src/modules/home/components/video-section/index.tsx
@@ -54,7 +54,7 @@ const VideoSection = () => {
             ) : (
               <div className="relative w-full h-full">
                  <video
-                   src="/_next/static/instagram/video2.mp4"
+                   src="/api/instagram/video2.mp4"
                    className="w-full h-full object-cover"
                    controls
                    autoPlay
